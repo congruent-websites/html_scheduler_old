1d571c95f49e13c362820e57bc9b1c7ef20941af
seniorDevMaster <seniordev119@gmail.com>
2020-10-05 22:05:15 -0500

    feedback v1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,31 @@
 var now = new Date()
-var cYear = now.getFullYear()
+var cYear = now.getUTCFullYear()
 var cMonth = now.getMonth() + 1
 var cDate = now.getDate()
 var cHour = now.getHours()
+var cDay = 0
+// var cMonth = now.getUTCMonth() + 1
+// var cDate = now.getUTCDate()
+// var cHour = now.getUTCHours()
 
-console.log('------------', cYear, now.getUTCFullYear(), cMonth, now.getUTCMonth(), cDate, now.getUTCDate())
-// var aDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+console.log(
+    "------------",
+    cYear,
+    now.getUTCFullYear(),
+    cMonth,
+    now.getUTCMonth(),
+    cDate,
+    now.getUTCDate(),
+    cHour,
+    now.getUTCHours()
+)
+
 var wDates = new Date(cYear, cMonth, 0).getDate()
 
 var lastWDates = 0
 var lastMonth = 0
 var lastDate = 0
 var pageIndex = 0
-var checkboxes = []
 
 var selectedTimes = []
 var selectedTimesLength = 0
@@ -31,6 +44,8 @@ for (var i = 0; i < 5; i++) {
         cMonth += 1
         cDate = 1
     }
+    // cDay = new Date(cYear, cMonth-1, cDate).getDay()
+    // console.log('cDay : ', cDay, cDay.getDay())
     curWeek(cMonth, cDate, i)
     lastMonth = cMonth
     lastDate = cDate
@@ -38,14 +53,15 @@ for (var i = 0; i < 5; i++) {
 }
 
 async function curWeek(currMonth, currDate, i) {
-    console.log('curWeek : ', currMonth, currDate, i)
+    console.log("curWeek : ", currMonth, currDate, i, new Date(cYear, currMonth, currDate), new Date(cYear, currMonth-1, currDate).getDay())
     await document
         .querySelector("#scheduleUl")
         .insertAdjacentHTML(
             "beforeend",
             `<li id="scheduleLi${currMonth}_${currDate}" tabindex="-1" class="Date-Option set Item${
                 i * 5 + 1
-            } active" data-row="${i * 5 + 1}" data-results="75" ></li>`
+            } active ${mobileCheck() && i === 0 ? 'current-day' : null}" data-row="${i * 5 + 1}" data-results="75" 
+            onclick="${mobileCheck() ? 'handleMobileScheduleDay('+`${currMonth}`+','+`${currDate}`+')' : null}"></li>`
         )
     await document
         .querySelector(`#scheduleLi${currMonth}_${currDate}`)
@@ -61,7 +77,7 @@ async function curWeek(currMonth, currDate, i) {
         )
     await document
         .querySelector(`#scheduleDiv${currMonth}_${currDate}`)
-        .insertAdjacentHTML("beforeend", `<strong>${aDays[i]}</strong>`)
+        .insertAdjacentHTML("beforeend", `<strong>${aDays[new Date(cYear, currMonth-1, currDate).getDay()]}</strong>`)
     await document
         .querySelector(`#scheduleLi${currMonth}_${currDate}`)
         .insertAdjacentHTML(
@@ -211,7 +227,7 @@ async function curWeek(currMonth, currDate, i) {
             '<span class="hide">Call For Service</span>'
         )
 }
-async function prevWeek(index) {
+async function prevWeek() {
     pageIndex--
     lastDate -= 10
 
@@ -222,11 +238,6 @@ async function prevWeek(index) {
     }
 
     document.querySelector("#nextBtn").classList.add("active")
-
-    checkboxes = checkboxes.concat(
-        document.querySelectorAll('input[type="checkbox"]:checked')
-    )
-
     document.querySelector("#scheduleUl").innerHTML = ""
 
     if (pageIndex === 0) {
@@ -243,23 +254,18 @@ async function prevWeek(index) {
         await curWeek(lastMonth, lastDate, i)
     }
 
-    console.log('prevWeek : ', checkboxes)
-    Array.prototype.forEach.call(checkboxes, function (elements) {
+    console.log("prevWeek : ", selectedTimes)
+    Array.prototype.forEach.call(selectedTimes, function (elements) {
         Array.prototype.forEach.call(elements, function (element) {
             if (document.querySelector(`#${element.id}`))
                 document.querySelector(`#${element.id}`).checked = true
         })
     })
 }
-async function nextWeek(index) {
+async function nextWeek() {
     pageIndex++
 
     document.querySelector("#prevBtn").classList.add("active")
-
-    checkboxes = checkboxes.concat(
-        document.querySelectorAll('input[type="checkbox"]:checked')
-    )
-
     document.querySelector("#scheduleUl").innerHTML = ""
 
     if (pageIndex > 3) {
@@ -275,9 +281,9 @@ async function nextWeek(index) {
         }
         await curWeek(lastMonth, lastDate, i)
     }
-    console.log('nextWeek : ', checkboxes)
+    console.log("nextWeek : ", selectedTimes)
 
-    Array.prototype.forEach.call(checkboxes, function (elements) {
+    Array.prototype.forEach.call(selectedTimes, function (elements) {
         Array.prototype.forEach.call(elements, function (element) {
             if (document.querySelector(`#${element.id}`))
                 document.querySelector(`#${element.id}`).checked = true
@@ -315,12 +321,9 @@ function createCustomerSelect() {
     })
 }
 function handleNext() {
-    checkboxes = checkboxes.concat(
-        document.querySelectorAll('input[type="checkbox"]:checked')
-    )
-    console.log(event, checkboxes)
+    console.log(selectedTimes)
 
-    if (checkboxes[0].length === 0) {
+    if (selectedTimes.length === 0) {
         Toastify({
             text: "Please Select A Time of Service.",
             duration: 5000,
@@ -332,7 +335,7 @@ function handleNext() {
             stopOnFocus: true, // Prevents dismissing of toast on hover
             onClick: function () {}, // Callback after click
         }).showToast()
-        checkboxes = []
+        selectedTimes = []
         return
     }
     document.querySelector("#mainSchedule").classList.remove("active")
@@ -352,20 +355,23 @@ function handleValidate(event) {
     }
 }
 function handleTimeSelect(event) {
-    if (event.target.checked)
-        selectedTimes.push(event.target)
+    if (event.target.checked) selectedTimes.push(event.target)
     else {
-        for (var i=0; i<selectedTimes.length; i++) {
-            if (selectedTimes[i].id === event.target.id) 
-                selectedTimes.splice(i, 1);
+        for (var i = 0; i < selectedTimes.length; i++) {
+            if (selectedTimes[i].id === event.target.id)
+                selectedTimes.splice(i, 1)
         }
-
     }
 
     if (selectedTimes.length > 2) {
         event.target.checked = false
         selectedTimes.pop()
     }
+}
+function handleMobileScheduleDay(month, day) {
+    console.log(month, day)
+    document.getElementsByClassName('current-day')[0].classList.remove('current-day')
+    document.querySelector(`#scheduleLi${month}_${day}`).classList.add('current-day')
 }
 function validateEmail(event) {
     var email = event.target.value
@@ -386,6 +392,23 @@ function onlyNumberKey(evt) {
     if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) return false
     return true
 }
+function mobileCheck() {
+    let check = false
+    ;(function (a) {
+        if (
+            /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+                a
+            ) ||
+            /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+                a.substr(0, 4)
+            )
+        )
+            check = true
+    })(navigator.userAgent || navigator.vendor || window.opera)
+
+    return check
+}
+
 document
     .getElementById("SchedulerLocalContactForm_ITM0_Phone")
     .addEventListener("input", function (e) {
@@ -396,3 +419,4 @@ document
             ? x[1]
             : "(" + x[1] + ") " + x[2] + (x[3] ? "-" + x[3] : "")
     })
+
